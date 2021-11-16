57f7c6a8f796a346ee6f20d493c2282bc658adb4
Gianine <gianine1508@gmail.com>
2021-11-16 15:23:25 -0300

    first commit - finalização do projeto

---
+++ script.js
@@ -0,0 +1,95 @@
+const notas = { 
+    100:1,
+    50:3,
+    20:4,
+    10:1,
+    5:3,
+    2:12,
+    1:10 
+}
+
+var saldo = 122.50
+
+const retira = document.querySelector("input")
+let aux = {}
+
+document.getElementById("bet").addEventListener('click',function() {
+    if(!validacaoSaldo(10,saldo)){
+        saque(notas,10,10)
+    }    
+},false);
+
+document.getElementById("vinte").addEventListener('click',function() {
+    if(!validacaoSaldo(20,saldo)){
+        saque(notas,20,20)
+    }    
+},false);
+
+document.getElementById("ciquenta").addEventListener('click',function() {
+    if(!validacaoSaldo(50,saldo)){
+        saque(notas,50,50)
+    }    
+},false);
+
+
+document.getElementById("cem").addEventListener('click',function() {
+    if(!validacaoSaldo(100,saldo)){
+        saque(notas,100,100)
+    }    
+},false);
+
+document.getElementById("duzentos").addEventListener('click',function() {
+    if(!validacaoSaldo(200,saldo)){
+        saque(notas,200,200)
+    }    
+},false);
+
+document.getElementById("trezentos").addEventListener('click',function() {
+    if(!validacaoSaldo(300,saldo)){
+        saque(notas,300,300)
+    }    
+},false);
+
+document.getElementById("saldo").textContent = "Saldo atual R$ " + saldo
+
+function validacaoSaldo(retirada,saldo){
+    if(retirada > saldo){
+        document.getElementById("result").textContent = "Saldo insuficiente para realizar retirada."
+        return true
+    }
+}
+
+document.getElementById("buttonRetirar").addEventListener('click',function() {
+    if(!validacaoSaldo(retira.value,saldo)){
+        saque(notas,parseInt(retira.value), parseInt(retira.value))
+    }    
+},false);
+
+function saque(notas,ret,valorInicial){
+    document.getElementById("result").textContent = ""
+    Object.keys(notas)
+        .reverse()
+        .map((k) => {
+            if(ret !== 0){
+                if ((k <= ret) && (notas[k] > 0)){
+                    for (notas[k]; notas[k] > 0; notas[k] --){
+                        if(ret >= k){
+                            ret = ret -k
+                            notas[k] = notas[k] -1
+                            console.log(notas[k])
+                        
+                            if(aux[k] === undefined ){
+                                aux[k] = 1
+                            }else{
+                                aux[k] += 1 ;
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        saldo = saldo - valorInicial
+
+    document.getElementById("saldo").innerHTML = "Saldo atual R$ " + saldo
+}
+
